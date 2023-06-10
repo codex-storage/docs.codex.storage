9bbe989c212106461a39b0e3f28fb4936b50c9a4
jinhojang6 <jinho5051@gmail.com>
2023-06-10 12:53:32 +0900

    update preset version and add terms

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,12 +4,14 @@ require('dotenv').config()
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'TITLE',
-  url: 'https://url',
+  title: 'Codex',
+  url: 'https://docs.codex.storage/',
   baseUrl: '/',
 
-  customFields: {
-    ghostAPiKey: process.env.GHOST_API_KEY,
+  customFields: {},
+
+  markdown: {
+    mermaid: true,
   },
 
   // Even if you don't use internalization, you can use this field to set useful
@@ -25,20 +27,81 @@ const config = {
       '@acid-info/logos-docusaurus-preset',
       /** @type {import('@acid-info/logos-docusaurus-preset').PluginOptions} */
       ({
-        businessUnit: 'Logos',
+        businessUnit: 'Codex',
         theme: {
           name: 'default',
           options: {
             customCss: [require.resolve('./src/css/custom.scss')],
+            docs: {
+              default: {
+                sidebar: {
+                  hide: false,
+                },
+              },
+            },
           },
+        },
+        docs: {
+          routeBasePath: '/',
         },
       }),
     ],
   ],
+  themes: ['@docusaurus/theme-mermaid'],
 
   themeConfig:
     /** @type {import('@acid-info/logos-docusaurus-preset').ThemeConfig} */
-    ({}),
+    ({
+      navbar: {
+        hideOnScroll: true,
+        items: [
+          {
+            type: 'search',
+          },
+        ],
+      },
+      footer: {
+        copyright: 'Codex @2023<br/>All Rights Reserved.',
+        links: [
+          {
+            items: [
+              {
+                href: 'https://twitter.com/Codex_storage',
+                label: 'Twitter',
+              },
+              {
+                href: 'https://discord.gg/2NXGrsqmDq',
+                label: 'Discord',
+              },
+              {
+                href: '/',
+                label: 'Docs',
+              },
+              {
+                href: 'https://github.com/codex-storage',
+                label: 'Github',
+              },
+            ],
+          },
+          {
+            items: [
+              {
+                href: '/',
+                label: 'Contact us',
+              },
+              {
+                href: 'https://jobs.status.im/',
+                label: 'Work with us',
+              },
+              {
+                href: '/terms',
+                label: 'Terms & conditions',
+              },
+            ],
+          },
+        ],
+      },
+    }),
 }
 
 module.exports = config
